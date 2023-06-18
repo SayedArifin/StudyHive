48026687735ae44351cda8b87324f8b9e9492cee
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2023-06-18 20:03:41 +0600

    beta -v3.5.2(bug-fixed)

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import "./home.css";
+import styles from "./Home.module.css";
 import hero from "../../public/heroSvg.svg";
 import Link from "next/link";
 import CourseList from "../../components/CourseList";
@@ -8,25 +8,25 @@ import CourseList from "../../components/CourseList";
 export default function Home() {
   return (
     <>
-      <div className="container">
-        <div className="svg-bg"></div>
-        <div className="content">
-          <div className="content-hero">
+      <div className={styles.container}>
+        <div className={styles.svg - bg}></div>
+        <div className={styles.content}>
+          <div className={styles.content - hero}>
             <h1>Unlock the Power of Coding with Our Up-to-Date Courses</h1>
-            <div className="content-secondary-text">
+            <div className={styles.content - secondary - text}>
               <p>
                 With our easy subscription model, you can get full access to all
                 of our courses. No need to hassle; we offer a 7-day money-back
                 guarantee.
               </p>
             </div>
-            <div className="content-button">
+            <div className={styles.content - button}>
               <Link href="#course">
                 <button>Browse Our Courses</button>
               </Link>
             </div>
           </div>
-          <div className="hero-img">
+          <div className={styles.hero - img}>
             <Image src={hero} alt="hero" />
           </div>
         </div>
