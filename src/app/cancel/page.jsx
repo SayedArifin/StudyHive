961a7892f1bd20644660b976a575f7d722185d23
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2023-06-19 20:26:53 +0600

    beta -v4.6(deployment bug fix)

--- src/app/cancel/page.jsx
+++ src/app/cancel/page.jsx
@@ -4,21 +4,20 @@ import Pricing from "../pricing/page";
 import { toast } from "react-toastify";
 
 const Success = () => {
+  const notifyOnSuccess = () => {
+    toast.success("Payment Cancelled", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
   useEffect(() => {
-    const notifyOnSuccess = () => {
-      toast.success("Payment Cancelled", {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-
-      notifyOnSuccess();
-    };
+    notifyOnSuccess();
   }, []);
 
   return (
