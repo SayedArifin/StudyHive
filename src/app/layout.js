3252772867af83fd7ff639504c4faffdb2791896
Sayed Arifin <111840910+SayedArifin@users.noreply.github.com>
2023-08-01 19:42:28 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,16 @@
+import Head from 'next/head';
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "../../components/Toastify";
-import Head from 'next/head';
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "StudyHive",
-  description:
-    "StudyHive is a dynamic and user-friendly Udemy clone project that aims to revolutionize online learning. With StudyHive, users can explore a vast collection of courses taught by expert instructors, covering diverse subjects ranging from programming and business to art and personal development. The platform offers a seamless learning experience, allowing students to enroll in courses, track their progress, and interact with instructors and fellow learners through an integrated discussion forum. StudyHive also includes features like personalized recommendations, course ratings and reviews, and a comprehensive dashboard for both students and instructors. With its intuitive interface and robust features, StudyHive empowers individuals to expand their knowledge and skills in a convenient and engaging manner.",
+  description: "StudyHive is a dynamic and user-friendly Udemy clone project that aims to revolutionize online learning...",
 };
-
 
 export default function RootLayout({ children }) {
   return (
@@ -26,7 +23,7 @@ export default function RootLayout({ children }) {
         />
         <script
           type="text/javascript"
-          src="https://chat4site.ai/embedWidget.js?t=" + Math.floor(Date.now())
+          src={"https://chat4site.ai/embedWidget.js?t=" + Math.floor(Date.now())}
           charSet="UTF-8"
           async
         ></script>
@@ -40,4 +37,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
